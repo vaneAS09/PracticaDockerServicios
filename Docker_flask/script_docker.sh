2778137975c90ededca4cdca001231a8ba28ad93
vaneAS09 <84061086+vaneAS09@users.noreply.github.com>
2022-03-16 16:21:14 -0500

    Add files via upload

---
+++ Docker_flask/script_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl status docker
+sudo docker info | more
+sudo docker build -t vane/flask .
+sudo docker run --name flasvane -d -p 9000:5000 vane/flask
+sudo docker container ls -a
